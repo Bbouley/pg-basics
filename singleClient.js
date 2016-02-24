0b84107f3ab2df316e80ec5e0a748cfd568070d6
bbouley <b.bouley@hotmail.co.uk>
2016-02-24 16:45:04 -0700

    removed examples on master branch

--- singleClient.js
+++ singleClient.js
@@ -1,27 +1,2 @@
-var pg = require('pg');
-var connectionString = 'postgres://localhost:5432/pgintro';
-var client = new pg.Client(connectionString);
-client.connect();
 
-// client.query("SELECT * FROM cities", function(err, result) {
-//     console.log(result.rows);
-// });
-
-// client.query("SELECT * FROM cities WHERE id=1", function(err, result) {
-//     console.log(result.rows);
-// });
-
-// client.query("SELECT * FROM cities WHERE id=2", function(err, result) {
-//     console.log(result.rows);
-// });
-
-
-function getAll () {
-    client.query("SELECT * FROM cities", function(err, result) {
-        console.log(result.rows);
-    });
-}
-
-getAll();
-client.end();
 
